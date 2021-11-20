399bb303cd63770ae18760df554eeb5b9b66f913
doubleZ <doublez@stu.pku.edu.cn>
2021-11-20 21:10:53 +0800

    :building_construction:fusibile add-in

--- MVSNet/eval.sh
+++ MVSNet/eval.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+# V100
 DTU_TESTING="/media/public/yan1/doublez/realdoubleZ/Data/MVS/test/dtu"
+# Lab
+# MVS_TRAINING="/home/doublez/Data/MVS/test/dtu"
+
 CKPT_FILE="./checkpoints/d192/model_000014.ckpt"
+
 CUDA_VISIBLE_DEVICES=2 python eval.py \
     --dataset=dtu_yao_eval \
     --batch_size=1 \
--- MVSNet/train.sh
+++ MVSNet/train.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+# V100
 MVS_TRAINING="/media/public/yan1/doublez/realdoubleZ/Data/MVS/train/dtu"
+# Lab
+# MVS_TRAINING="/home/doublez/Data/MVS/train/dtu"
+
 CUDA_VISIBLE_DEVICES=2,3,4 python train.py \
     --dataset=dtu_yao \
     --batch_size=6 \
@@ -7,4 +11,5 @@ CUDA_VISIBLE_DEVICES=2,3,4 python train.py \
     --trainlist lists/dtu/train.txt \
     --testlist lists/dtu/test.txt \
     --numdepth=192 \
+    --resume \
     --logdir ./checkpoints/d192 $@
