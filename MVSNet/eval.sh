e175103482a3f718c7b98144595975180f4f30c8
doubleZ <doublez@stu.pku.edu.cn>
2021-11-26 09:30:01 +0800

    save from v100

--- MVSNet/eval.sh
+++ MVSNet/eval.sh
@@ -4,11 +4,11 @@ DTU_TESTING="/media/public/yan1/doublez/realdoubleZ/Data/MVS/test/dtu"
 # Lab
 # MVS_TRAINING="/home/doublez/Data/MVS/test/dtu"
 
-CKPT_FILE="./checkpoints/d192/model_000014.ckpt"
+CKPT_FILE="./checkpoints/test1122/model_000000.ckpt"
 
 CUDA_VISIBLE_DEVICES=7 python eval.py \
     --dataset=dtu_yao_eval \
     --batch_size=1 \
     --testpath=$DTU_TESTING \
-    --testlist lists/dtu/test.txt \
+    --testlist lists/dtu/mytest.txt \
     --loadckpt $CKPT_FILE $@
